f3bd552ec585e048eefa13e8eff982d84c951bb4
Gleb Lugovtsov <galera951@gmail.com>
2024-10-16 12:21:42 +0300

    Init telegram client and add e.Wrap()

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"read-tip-bot/clients/telegram"
+)
+
+
+func main() {
+	host, token := mustParseFlags()
+	_ = telegram.New(host, token)
+
+	// fetcher = fetcher.New()
+
+	// processor = processor.New()
+
+	//consumer.Start(fetcher, processor)
+}
+
+func mustParseFlags() (string, string) {
+	host := flag.String("h", "", "telegram api host")
+	token := flag.String("t", "", "token for access to telegram bot")
+
+	flag.Parse()
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+	if *host == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *host, *token
+}
